2716c73aa59fb3a58ce04101bf1608fc599d0e3c
Justin Braben <jbraben@dmsiworks.com>
2023-11-27 22:50:57 -0700

    once more

--- src/core/factories.hpp
+++ src/core/factories.hpp
@@ -4,4 +4,4 @@
 #include <SFML/Graphics.hpp>
 
 entt::entity makePlayer(entt::registry& reg, sf::Sprite& playerSprite);
-entt::entity makePipe(entt::registry& reg, sf::Sprite& sprite, sf::Vector2f& pos, sf::Vector2f& velocity);
+entt::entity makePipe(entt::registry& reg, sf::Sprite& sprite, sf::Vector2f pos, sf::Vector2f velocity);
